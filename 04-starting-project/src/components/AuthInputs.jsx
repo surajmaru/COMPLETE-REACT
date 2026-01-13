4e45644acb71f381fb8446ed6af9128dc3f5ee3b
surajmaru <jakejonathan69@gmail.com>
2026-01-14 04:01:53 +0530

    .

--- 04-starting-project/src/components/AuthInputs.jsx
+++ 04-starting-project/src/components/AuthInputs.jsx
@@ -1,16 +1,8 @@
 import { useState } from 'react';
-import {styled} from "styled-components";
 
 import Button from './Button.jsx';
 
 import Input from './Input.jsx';
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -33,14 +25,14 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 rounded shadow-md bg-gradient-to-b from-stone-600 to-stone-800 mx-auto'>
+      <div className='flex flex-col gap-2 mb-6'>
 
           <Input
             label="Email"
             invalid={emailNotValid}
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
 
@@ -48,18 +40,18 @@ export default function AuthInputs() {
             label="Password"
             invalid={passwordNotValid}
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
 
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <Button className='button' onClick={handleLogin}>Sign In</Button>
+        <Button  onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
--- 04-starting-project/src/components/Input.jsx
+++ 04-starting-project/src/components/Input.jsx
@@ -1,30 +1,21 @@
-import {styled} from "styled-components"
+export default function Input({label, invalid, ...props}){
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${ (props) => props.$invalid ? "#f87171" : "#6b7280" };
-` 
+  let labelClass = "block mb-2 text-xs font-bold tracking-wide uppercase";
+  let inputClass = "w-full px-3 py-2 leading-tight border rounded shadow";
 
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${ (props) => props.$invalid ? "#fed2d2" : "#d1d5db" };
-  color: ${ (props) => props.$invalid ? "#ef4444" : "#374151" };
-  border: 1px solid ${ (props) => props.$invalid ? "#f73f3f" : "transparent" };
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
-export default function CustomInput({label, invalid, ...props}){
+  if(invalid){
+    labelClass += " text-red-400";
+    inputClass += " text-red-500 bg-red-100 border-red-300";
+  } else{
+    labelClass += " text-stone-300";
+    inputClass += " text-gray-700 bg-stone-300";
+  }
+
+
     return(
         <p>
-            <Label $invalid={invalid}>{label}</Label>
-            <Input $invalid={invalid} {...props} />
+      <label className={labelClass}>{label}</label>
+      <input className={inputClass} {...props} />
         </p>
     )
 }
