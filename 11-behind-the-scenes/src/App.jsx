5a2e76a7a0354a84c968e5326c76fe1fc6989945
Suraj <suraj123maru@gmail.com>
2026-05-15 01:28:37 +0530

    section complete

--- 11-behind-the-scenes/src/App.jsx
+++ 11-behind-the-scenes/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Header />
       <main>
         <ConfigureCounter onSetCount={handleSetCount} />
-        <Counter initialCount={chosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
       </main>
     </>
   );
