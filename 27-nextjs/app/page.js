11d25470953c0326699328ba073f87b314f3c8e0
Suraj <suraj123maru@gmail.com>
2026-06-13 16:03:06 +0530

    next-app setup

---
+++ 27-nextjs/app/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Header from "@/components/header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">About us!!</Link>
+      </p>
+    </main>
+  );
+}
